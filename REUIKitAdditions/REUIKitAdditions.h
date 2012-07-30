a62c3c882a41b30d467336b9a81a0f54e2cab2f0
Roman Efimov <romaonthego@gmail.com>
2012-07-30 14:04:18 -0500

    Move macros to a separate file

--- REUIKitAdditions/REUIKitAdditions.h
+++ REUIKitAdditions/REUIKitAdditions.h
@@ -26,6 +26,4 @@
 #import <Foundation/Foundation.h>
 #import "UIView+REUIKitAdditions.h"
 #import "UIImageView+REUIKitAdditions.h"
-
-#define RGB(r, g, b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
-#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
+#import "Macros.h"
