e7959b3c12af11ad5e87e2a6a602bb5ae894c1c2
Roman Efimov <romaonthego@gmail.com>
2012-07-30 14:04:56 -0500

    Add notes

--- REUIKitAdditions/Macros.h
+++ REUIKitAdditions/Macros.h
@@ -1,3 +1,10 @@
+//
+// Macros.h
+// REUIKitAdditions
+//
+
+
+// UIColor macros
 // http://cocoamatic.blogspot.com/2010/07/uicolor-macro-with-hex-values.html
 #define RGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #define RGBA(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
