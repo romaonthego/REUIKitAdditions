677c1b608c75403afd614f0d7827bb9dbfd0105c
Roman Efimov <romaonthego@gmail.com>
2012-08-27 14:03:45 -0500

    Bugfix

--- REUIKitAdditions/REUIKitMacros.h
+++ REUIKitAdditions/REUIKitMacros.h
@@ -7,18 +7,18 @@
 // UIColor
 // Source: http://cocoamatic.blogspot.com/2010/07/uicolor-macro-with-hex-values.html
 //
-#ifndef
+#ifndef RGB
   #define RGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 #endif
 
-#ifndef
+#ifndef RGBA
   #define RGBA(rgbValue, a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 #endif
 
-#ifndef
+#ifndef RGB2
   #define RGB2(r, g, b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
 #endif
 
-#ifndef
+#ifndef RGBA2
   #define RGBA2(r, g, b, a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #endif
