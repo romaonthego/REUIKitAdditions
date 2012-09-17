017d38b0f8572902fe6dead4bf5a8ea42b58a915
Roman Efimov <romaonthego@gmail.com>
2012-09-17 12:57:10 -0500

    Swizzle Retina 4 images

--- REUIKitAdditions/UIImage+REUIKitAdditions.m
+++ REUIKitAdditions/UIImage+REUIKitAdditions.m
@@ -24,8 +24,47 @@
 //
 
 #import "UIImage+REUIKitAdditions.h"
+#import <objc/runtime.h>
+#import <objc/message.h>
+
+static Method origImageNamedMethod = nil;
 
 @implementation UIImage (REUIKitAdditions)
+
++ (void)initialize
+{
+    origImageNamedMethod = class_getClassMethod(self, @selector(imageNamed:));
+    method_exchangeImplementations(origImageNamedMethod,
+                                   class_getClassMethod(self, @selector(retina4ImageNamed:)));
+}
+
+//  Created by Benjamin Stahlhood on 9/12/12.
+//  Copyright (c) 2012 DS Media Labs. All rights reserved.
++ (UIImage *)retina4ImageNamed:(NSString *)imageName
+{
+    NSMutableString *imageNameMutable = [imageName mutableCopy];
+    NSRange retinaAtSymbol = [imageName rangeOfString:@"@"];
+    if (retinaAtSymbol.location != NSNotFound) {
+        [imageNameMutable insertString:@"-568h" atIndex:retinaAtSymbol.location];
+    } else {
+        CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+        if ([UIScreen mainScreen].scale == 2.f && screenHeight == 568.0f) {
+            NSRange dot = [imageName rangeOfString:@"."];
+            if (dot.location != NSNotFound) {
+                [imageNameMutable insertString:@"-568h@2x" atIndex:dot.location];
+            } else {
+                [imageNameMutable appendString:@"-568h@2x"];
+            }
+        }
+    }
+    NSString *imagePath = [[NSBundle mainBundle] pathForResource:imageNameMutable ofType:@"png"];
+    if (imagePath) {
+        return [UIImage retina4ImageNamed:imageNameMutable];
+    } else {
+        return [UIImage retina4ImageNamed:imageName];
+    }
+    return nil;
+}
 
 + (UIImage *)maskedImageNamed:(NSString *)name color:(UIColor *)color
 {
